02c613ae03e165b0ab22efa84a8dfe8364237ba5
rajubd <raju.rayhan@yandex.com>
2019-09-08 17:45:14 +0600

    Edited

--- index.php
+++ index.php
@@ -16,10 +16,6 @@
 
 	$object 	= new Custom();	
 
-	// $object->name   	= 'Sample Name';
-	// $object->mobile   	= '01956533217';
-	// $object->email   	= 'raju@adboxbd.com';
-
 	$object->offsetSet('name', 'Sample Name');
 	$object->offsetSet('mobile', '01849699001');
 	$object->offsetSet('email', 'devraju.bd@gmail.com');
