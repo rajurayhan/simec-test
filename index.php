ed9d6f1cb1b45d6b6450e9fba58c24faa19fb7e9
rajubd <raju.rayhan@yandex.com>
2019-09-11 10:26:08 +0600

    Single File

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
 
-	include "custom.class.php";
+class ArrayObj extends ArrayObject
+{
 
-	// Part One
-	
-	$cObj = new Custom();
+	public function showTable()
+	{
+		function table( $data ) {
+			$html = '';
+			foreach( $data as $key => $value ) {
+				if ( is_array( $value ) ) {
+					$html .= sprintf( '<tr><td>%s</td><td>%s</td></tr>', $key, table( $value ) );
+				} else {
+					$html .= sprintf( '<tr><td>%s</td><td>%s</td></tr>', $key, $value );
+				}
+			}
+			$html = sprintf( '<table style="width:100%%;border-collapse:collapse;" border="1"><tr><th>Key</th><th>Value</th></tr>%s</table>', $html );
+			return $html;
+		};
+		echo table( $this->getIterator() );
+	}
 
-	echo 'Part One: <br>';
+}
 
-	$cObj->showTable();	
-
-	echo '<br>Part Two: <br>';
-
-	// Part Two
-
-	$object 	= new Custom();	
-
-	$object->offsetSet('name', 'Sample Name');
-	$object->offsetSet('mobile', '01849699001');
-	$object->offsetSet('email', 'devraju.bd@gmail.com');
-
-	$object->showTable();	
-?>
+$arrayObj = new ArrayObj();
+$arrayObj->append( [ 'key01' => 'value01' ] );
+$arrayObj->offsetSet( 'key02', 'value02' );
+$arrayObj->offsetSet( 'key03', [ 'key04' => 'value04' ] );
+$arrayObj->offsetSet( 'key05', [ 'key06' => [ 'key07' => 'value07' ], 'value08' ] );
+$arrayObj->showTable();
